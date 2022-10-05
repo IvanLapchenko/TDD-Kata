6985222c66c79155866e43510400b54551fd443f
Ivan <lapchenko.vanya@gmail.com>
2022-10-05 09:47:34 +0300

    First commit with test

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TDD_Kata
+{
+    class StringCalculator
+    {
+        public int Add(string numbers)
+        {
+            if (numbers == "")
+                return 0;
+            return 0; 
+            
+        }
+    }
+
+    class Program
+    {
+        public static void Main()
+        {
+
+        }
+    }
+}
---
+++ TestClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace TDD_Kata
+{
+    [TestFixture]
+    internal class TestClass
+    {
+        [Test]
+        public void TakesVoidReturnsZero()
+        {
+            StringCalculator stringCalculator = new();
+            Assert.AreEqual(0, stringCalculator.Add(""));
+        }
+
+    }
+}
